be19b45a16391f59305d613eb542b55b13ee17e3
Dor <108623675+D0rDa4aN919@users.noreply.github.com>
2023-08-15 13:13:16 +0300

    Update create_json.py

--- create_json.py
+++ create_json.py
@@ -1,3 +1,10 @@
+##############################################################################
+# Offline Py-JSON-Dropper - create_json - Python Script
+# Description: A POC for JSON dropper(drop by fetching API JSON). It will ask for malware path and create a new json file with encode malware bodies.
+# Author: Dor Dahan
+# License: MIT (See details in the LICENSE file or at the end of this script)
+##############################################################################
+
 import base64, json
 
 class Creator:
@@ -24,8 +31,7 @@ class Creator:
         self.names.append(binary_file)
         encoded = base64.b32encode(text)
         encoded = encoded.decode('utf-8')
-        self.final_json[binary_file.split(".")[0]] = {"body": [seg for seg in encoded[::4]], "fullName": binary_file}
-
+        self.final_json[binary_file.split(".")[0]] = {"body": encoded, "fullName": binary_file}
 
     def save_json(self, file_name:str) -> None:
         """
@@ -74,3 +80,25 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# License Information
+# This script is open-source and released under the MIT License.
+# MIT License
+# Copyright (c) 2023 Dor Dahan
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# For more details, see the LICENSE file in the root directory of this repository
+# or visit https://github.com/D0rDa4aN919/Py-JSON-Dropper.
